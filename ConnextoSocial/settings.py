802b53768f08fa62a8f96db529a2488cf797cb68
Rafael Petrov <djfanta@abv.bg>
2024-12-11 22:27:54 +0200

    feat: fixed settings.py

--- ConnextoSocial/settings.py
+++ ConnextoSocial/settings.py
@@ -12,7 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', config('SECRET_KEY'))
-# django-insecure-5hfl3*4yfz^8uqg&mj9w6t@hz14ef9rt4i3chuo2z76vd+52o^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
